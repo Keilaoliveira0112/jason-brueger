f65cd3e832036bdd72dabfebd14fa9fa089d8272
AdrianaKatarina <dricakatarina@gmail.com>
2023-05-22 10:34:17 -0300

    feat(login/breakfast):route creation and redirection

--- src/Components/Forms/Form.jsx
+++ src/Components/Forms/Form.jsx
@@ -1,12 +1,14 @@
 import Button from '../button/Button';
 import Input from '../input/Input';
 import { CreateForm } from "./Form.styles";
+import { useNavigate } from "react-router-dom";
 
 const Form = () => {
+  const navigation = useNavigate();
     return (
         <CreateForm>
           <Input />
-          <Button />
+          <Button text={'Entrar'} onClick={() => navigation('/breakfast')}/>
         </CreateForm>
     )
   }
