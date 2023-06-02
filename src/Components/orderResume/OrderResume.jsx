de7703955e44ad06f369cf84f139ad6801ecca73
Keila <keilaoliveira.copy@gmail.comm>
2023-06-02 20:23:33 -0300

    feat(breakfast): merge

--- src/Components/orderResume/OrderResume.jsx
+++ src/Components/orderResume/OrderResume.jsx
@@ -3,28 +3,29 @@ import btnDelete from '../../assets/btnDelete.svg'
 
 
 const OrderResume = (props) => {
-  return (
-    <Section>
-      <Title>Resumo da Lápide</Title>
-      <p>Cova: {props.selectValue}</p>
-      <ul>
-        {props.orderItem.map((item)=> (
-          <li key={item.id}>
-            <h4>{item.name}</h4>
-            <p>{`R$${item.price * item.quantity}`}</p>
-            <button onClick={() => props.onClickQuantity(item, '-')}>-</button>
-            <p>{item.quantity}</p>
-            <button onClick={() => props.onClickQuantity(item, '+')}>+</button>
-            <button onClick={() => props.onClick(item)}>
-              <img src={btnDelete} alt='excluir item do pedido'/>
-            </button>
-          </li>
-        ))}
-        
-      </ul>
-      <h3>Total: R$</h3>
-    </Section>
-  )
+    return (
+        <Section>
+            <Title>Resumo da Lápide</Title>
+            <p>Cova: {props.selectValue}</p>
+            <p>Nome do Cliente: {props.clientNameValue}</p>
+            <ul>
+                {props.orderItem.map((item) => (
+                    <li key={item.id}>
+                        <h4>{item.name}</h4>
+                        <p>{`R$${item.price * item.quantity}`}</p>
+                        <button onClick={() => props.onClickQuantity(item, '-')}>-</button>
+                        <p>{item.quantity}</p>
+                        <button onClick={() => props.onClickQuantity(item, '+')}>+</button>
+                        <button onClick={() => props.onClick(item)}>
+                            <img src={btnDelete} alt='excluir item do pedido' />
+                        </button>
+                    </li>
+                ))}
+            </ul>
+            <h3>Total: R${props.total}</h3>
+            <button>Enviar</button>
+        </Section>
+    )
 }
-  
+
 export default OrderResume;
--- src/Paginas/attendant/restOfTheDay/RestOfTheDay.jsx
+++ src/Paginas/attendant/restOfTheDay/RestOfTheDay.jsx
@@ -97,6 +97,7 @@ const RestOfTheDay = () => {
     }
   }
 
+
   return (
       <>
         <Header />
