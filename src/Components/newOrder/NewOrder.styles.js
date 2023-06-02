6d6a4098d5892b267314bfa62798bf9dbe610e4a
AdrianaKatarina <dricakatarina@gmail.com>
2023-06-02 19:49:20 -0300

    style(attendant): styling pages

---
+++ src/Components/newOrder/NewOrder.styles.js
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+
+export const Main = styled.main `
+display: flex;
+flex-direction: column;
+align-items: center;
+gap: 3rem;
+padding: 3rem;
+@media only screen and (min-width: 600px) {
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: flex-start;
+}
+
+`
+export const SectionMenu = styled.section`
+display: grid;
+gap: 2rem;
+@media only screen and (min-width: 1024px) {
+  width: 32vw;
+}
+`
+export const TitleMenu = styled.h3`
+text-align: center;
+justify-self: center;
+background: #ffff;
+width: 40vw;
+height: 3vh;
+font-size: 1.3rem;
+font-family: 'Rubik Wet Paint', cursive;
+background-color: #3A0504;
+color: #fff;
+font-weight: 100;
+border-radius: 0.5rem;
+@media only screen and (min-width: 600px) {
+  font-size: 1.8rem;
+  width: 28vw;
+  height: 3vh;
+  padding: 0.5rem;
+}
+@media only screen and (min-width: 1024px) {
+  width: 18vw;
+  height: 3.5vh;
+}
+`
+
+export const UlMenu = styled.ul`
+display: grid;
+gap: 1rem;
+`
