cc466c63f3ab640ccab07802d3053c4d99bfaadd
Keila <keilaoliveira.copy@gmail.comm>
2023-06-20 14:05:09 -0300

    merge

--- src/Components/select/Select.jsx
+++ src/Components/select/Select.jsx
@@ -2,8 +2,8 @@ import { SelectContainer } from "./Select.styles"
 
  const Select = (props) => {
   return (
-      <SelectContainer defaultValue={{value: ''}} onChange={props.onChange}>
-          <option value="" >Cova</option>
+      <SelectContainer defaultValue={{value: 'Cova'}} onChange={props.onChange}>
+          <option value="Cova" >Cova</option>
           <option value="001">Cova 1</option>
           <option value="002">Cova 2</option>
           <option value="003">Cova 3</option>
