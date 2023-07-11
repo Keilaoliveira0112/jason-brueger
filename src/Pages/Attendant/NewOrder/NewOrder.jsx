5e6df2952e7846139da32bf71a8d1c5e0ec02348
AdrianaKatarina <dricakatarina@gmail.com>
2023-07-11 18:10:13 -0300

    refactor(ContainerButtons/NewOrder): code review

--- src/Pages/Attendant/NewOrder/NewOrder.jsx
+++ src/Pages/Attendant/NewOrder/NewOrder.jsx
@@ -27,7 +27,29 @@ const NewOrder = () => {
   const [typeModal, setTypeModal] = useState("");
   const [modalMessage, setmodalMessage] = useState("");
   const [valueArguments, setvalueArguments] = useState([]);
-  const [productType, setProductType] = useState("Breakfast");
+  const [timeOfDay, setTimeOfDay] = useState("Breakfast");
+
+  const openSuccessModal = (message) => {
+    // setOpenModalUpdate(false);
+    setmodalMessage(message);
+    setTypeModal("sucess");
+    setOpenModal(true);
+    setTimeout(() => { setOpenModal(false); }, 3000);
+  };
+
+  const openWarningModal = (message) => {
+    // setOpenModalUpdate(false);
+    setmodalMessage(message);
+    setTypeModal("warning");
+    setOpenModal(true);
+  };
+
+  const openConfirmationModal = (message) => {
+    // setOpenModalUpdate(false);
+    setmodalMessage(message);
+    setTypeModal("confirmation");
+    setOpenModal(true);
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -35,9 +57,7 @@ const NewOrder = () => {
         const response = await getProducts();
         setProducts(response);
       } catch (error) {
-        setmodalMessage(error.message);
-        setTypeModal("warning");
-        setOpenModal(true);
+        openWarningModal(error.message);
       }
     };
     fetchData();
@@ -49,18 +69,16 @@ const NewOrder = () => {
     navigation(page);
   };
 
-  const handleClick = (e) => {
+  const handleClickProductType = (e) => {
     e.preventDefault();
-    const type = e.target.textContent === "Resto do dia" ? "RestOfTheDay" : "Breakfast";
-    setProductType(type);
+    const type = e.target.textContent === "Café da manhã" ? "Breakfast" : "RestOfTheDay";
+    setTimeOfDay(type);
   };
 
   const handleClickDelete = (item) => {
     if (!openModal) {
       setvalueArguments(item);
-      setmodalMessage("Tem certeza que deseja excluir esse item do resumo de pedidos?");
-      setTypeModal("confirmation");
-      setOpenModal(true);
+      openConfirmationModal("Tem certeza que deseja excluir esse item do resumo de pedidos?");
     } else {
       const getIndex = orderItem.findIndex((order) => order.id === valueArguments.id);
       const newOrder = [...orderItem];
@@ -82,23 +100,16 @@ const NewOrder = () => {
       }
       if (!openModal) {
         setvalueArguments(orderTotal);
-        setmodalMessage("Confirma o envio do pedido para a cozinha?");
-        setTypeModal("confirmation");
-        setOpenModal(true);
+        openConfirmationModal("Confirma o envio do pedido para a cozinha?");
       } else {
         await createOrder(valueArguments, selectValue, orderItem, clientName);
-        setmodalMessage("Pedido enviado com sucesso");
-        setTypeModal("sucess");
-        setOpenModal(true);
-        setTimeout(() => { setOpenModal(false); }, 3000);
+        openSuccessModal("Pedido enviado com sucesso");
         setOrderItem([]);
         setName("");
         setSelectValue("");
       }
     } catch (error) {
-      setmodalMessage(error.message);
-      setTypeModal("warning");
-      setOpenModal(true);
+      openWarningModal(error.message);
     }
   };
 
@@ -124,22 +135,18 @@ const NewOrder = () => {
   const handleClickQuantity = (item, children) => {
     const getIndex = orderItem.findIndex((order) => order.id === item.id);
     const newOrder = [...orderItem];
+    const specificItem = newOrder[getIndex];
     if (children === "-") {
       if (item.quantity <= 1) {
         handleClickDelete(item);
       } else {
-        const specificItem = newOrder[getIndex];
-        const valueChange = specificItem.quantity - 1;
-        newOrder[getIndex].quantity = valueChange;
-        setOrderItem(newOrder);
+        newOrder[getIndex].quantity = specificItem.quantity - 1;
       }
     }
     if (children === "+") {
-      const specificItem = newOrder[getIndex];
-      const quantityChange = specificItem.quantity + 1;
-      newOrder[getIndex].quantity = quantityChange;
-      setOrderItem(newOrder);
+      newOrder[getIndex].quantity = specificItem.quantity + 1;
     }
+    setOrderItem(newOrder);
   };
 
   const handleAddItems = (product) => {
@@ -150,9 +157,7 @@ const NewOrder = () => {
       if (!verification) {
         setOrderItem((prevState) => [...prevState, product]);
       } else {
-        setmodalMessage("Produto já foi adicionado no resumo! Caso queira alterar a quantidade, usar os botões no resumo do pedido");
-        setTypeModal("warning");
-        setOpenModal(true);
+        openWarningModal("Produto já foi adicionado no resumo! Caso queira alterar a quantidade, usar os botões no resumo do pedido");
       }
     }
   };
@@ -174,23 +179,14 @@ const NewOrder = () => {
       />
       <Main>
         <SectionMenu>
-          {productType === "RestOfTheDay" ? (
-            <ContainerButtons
-              variantBtnOne="tertiary"
-              variantBtnTwo="secondary"
-              onClickBtnOne={handleClick}
-              childrenBtnTwo="Resto do dia"
-              childrenBtnOne="Café da manhã"
-            />
-          ) : (
-            <ContainerButtons
-              variantBtnOne="secondary"
-              variantBtnTwo="tertiary"
-              onClickBtnTwo={handleClick}
-              childrenBtnTwo="Resto do dia"
-              childrenBtnOne="Café da manhã"
-            />
-          )}
+          <ContainerButtons
+            variantBtnOne={timeOfDay === "Breakfast" ? "secondary" : "tertiary"}
+            variantBtnTwo={timeOfDay === "RestOfTheDay" ? "secondary" : "tertiary"}
+            onClickBtnOne={handleClickProductType}
+            onClickBtnTwo={handleClickProductType}
+            childrenBtnOne="Café da manhã"
+            childrenBtnTwo="Resto do dia"
+          />
           <Input
             type="text"
             value={clientName}
@@ -203,7 +199,7 @@ const NewOrder = () => {
             defaultValue="Cova"
             optionValues={["001", "002", "003", "004"]}
           />
-          {productType === "RestOfTheDay" ? (
+          {timeOfDay === "RestOfTheDay" ? (
             <>
               <TitleMenu>Hamburguers</TitleMenu>
               <UlMenu>
@@ -228,7 +224,7 @@ const NewOrder = () => {
                         key={product.id}
                         name={product.name}
                         price={`R$${product.price}`}
-                        onClick={() => setOrderItem((prevState) => [...prevState, product])}
+                        onClick={() => handleAddItems(product)}
                       />
                     );
                 })}
@@ -242,7 +238,7 @@ const NewOrder = () => {
                         key={product.id}
                         name={product.name}
                         price={`R$${product.price}`}
-                        onClick={() => setOrderItem((prevState) => [...prevState, product])}
+                        onClick={() => handleAddItems(product)}
                       />
                     );
                 })}
