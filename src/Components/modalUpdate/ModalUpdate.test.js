39e0a1c61a34de3ff6e5671ccacd97fbbd6f4d21
Keila <keilaoliveira.copy@gmail.comm>
2023-07-14 19:57:24 -0300

    feat(ReadyOrders): new resume

---
+++ src/Components/modalUpdate/ModalUpdate.test.js
@@ -0,0 +1,11 @@
+import ModalUpdate from "./ModalUpdate";
+import { render, screen } from '@testing-library/react';
+import userEvent from "@testing-library/user-event";
+
+describe("<ModalUpdate />", () => {
+    it("Should render the Modal component and perform its actions correctly when typeModal is 'confirmation'", () => {
+        const props = {
+            isOpen: true, 
+        }
+    })
+})
